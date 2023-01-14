99b7fdd11e063c39057aaacaacdbfa68f86da7cd
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2023-01-14 14:10:21 -0300

    update activity

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -4,17 +4,15 @@ package com.example.noteapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.noteapp.databinding.ActivityMainBinding
-import com.example.noteapp.databinding.ActivityMainBinding.inflate
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
